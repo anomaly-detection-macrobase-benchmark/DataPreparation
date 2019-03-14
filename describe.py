a3aba3daa7c5b69abd72338f2052abd5225f0d19
Alex P <alex.pantec@gmail.com>
2019-03-14 10:54:00 +0200

    describe

---
+++ describe.py
@@ -0,0 +1,25 @@
+import argparse
+import pandas as pd
+
+
+parser = argparse.ArgumentParser(
+    description='''Prints dataset info''',
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    epilog='''Example:
+describe.py output/index.json
+describe.py output/index.json --label is_anomaly''')
+parser.add_argument('file', type=str, help='path to the CSV file')
+parser.add_argument('--label', metavar='label_column', dest='label_column_name', type=str, help='label column name')
+args = parser.parse_args()
+
+df = pd.read_csv(args.file, sep=',')
+
+print(df)
+pd.set_option('display.max_columns', 500)
+print(df.describe(include='all'))
+
+if (args.label_column_name):
+    print('\nLabel column: ')
+    label_column = df[args.label_column_name]
+    print('Value    %')
+    print(label_column.value_counts(normalize=True) * 100)

a4f63f8947a84063aabc375a7bbbd51aa12edbde
Alex P <alex.pantec@gmail.com>
2019-03-14 12:05:08 +0200

    rename

--- describe.py
+++ describe.py
@@ -3,15 +3,15 @@ from utils.argparse import ArgParser
 import pandas as pd
 
 
-parser = ArgParser(
+arg_parser = ArgParser(
     description='''Prints dataset info''',
     formatter_class=argparse.RawDescriptionHelpFormatter,
     epilog='''Example:
 describe.py output/index.json
 describe.py output/index.json --label is_anomaly''')
-parser.add_argument('file', type=str, help='path to the CSV file')
-parser.add_argument('--label', metavar='label_column', dest='label_column_name', type=str, help='label column name')
-args = parser.parse_args()
+arg_parser.add_argument('file', type=str, help='path to the CSV file')
+arg_parser.add_argument('--label', metavar='label_column', dest='label_column_name', type=str, help='label column name')
+args = arg_parser.parse_args()
 
 df = pd.read_csv(args.file, sep=',')
 

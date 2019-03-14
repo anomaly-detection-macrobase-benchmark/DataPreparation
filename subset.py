ef565b78f0f1d7157d486476fdb917e151964c63
Alex P <alex.pantec@gmail.com>
2019-03-14 16:30:18 +0200

    simplify size input

--- subset.py
+++ subset.py
@@ -7,10 +7,11 @@ arg_parser = ArgParser(
     description='''Splits dataset into random subsets of specified size''',
     formatter_class=argparse.RawDescriptionHelpFormatter,
     epilog='''Example:
-subset.py original_datasets/shuttle-unsupervised-ad.csv output/shuttle_{size}.csv 5000,10000,15000,20000,25000,30000,35000''')
+subset.py original_datasets/shuttle-unsupervised-ad.csv output/shuttle_{size}.csv 10000,20000,30000,40000
+subset.py original_datasets/shuttle-unsupervised-ad.csv output/shuttle_{size}.csv 10K,20K,30K,40K''')
 arg_parser.add_argument('file', type=str, help='path to the CSV file')
 arg_parser.add_argument('output_file', type=str, help='path to the CSV file')
-arg_parser.add_argument('sizes', type=lambda s: [int(item) for item in s.split(',')], help='sizes')
+arg_parser.add_argument('sizes', type=lambda s: [sz.parse_size(item) for item in s.split(',')], help='sizes')
 args = arg_parser.parse_args()
 
 df = load_csv(args.file)
--- utils/sizes.py
+++ utils/sizes.py
@@ -1,6 +1,16 @@
+units = ['', 'K', 'M']
+
+
 def format_size(num):
-    for unit in ['','K']:
+    for unit in units[:-1]:
         if abs(num) < 1000:
             return "%d%s" % (num, unit)
         num /= 1000
-    return "%d%s" % (num, 'M')
+    return "%d%s" % (num, units[-1])
+
+
+def parse_size(s):
+    s = s.upper()
+    for unit in units[1:]:
+        s = s.replace(unit, '000')
+    return int(s)

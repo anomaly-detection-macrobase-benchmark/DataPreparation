7a0a656b4f62ea5321a6661e5276ccf5f0d01d97
Alex P <alex.pantec@gmail.com>
2019-04-06 01:20:54 +0300

    create dir

--- utils/plots.py
+++ utils/plots.py
@@ -4,6 +4,8 @@ import os
 
 import matplotlib.pyplot as plt
 
+from utils.fs import create_dirs_if_not_exist
+
 
 def color_cycle():
     return itertools.cycle(plt.rcParams['axes.prop_cycle'].by_key()['color'])
@@ -14,6 +16,7 @@ def marker_cycle():
 
 
 def save_plot(fig, name, dir, name_format='{dt}_{suptitle}_{name}', image_format='png'):
+    create_dirs_if_not_exist(dir)
     fig.savefig(os.path.join(dir, name_format.format(
         dt=datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"),
         name=name,

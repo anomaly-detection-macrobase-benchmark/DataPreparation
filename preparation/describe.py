b8d58811321d5e948bb4a011eab84721a08d5a0d
Alex P <alex.pantec@gmail.com>
2019-06-17 09:50:26 +0300

    reduce code duplication

--- preparation/describe.py
+++ preparation/describe.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 from utils.argparse import ArgParser
+from utils.datasets import load_stats
 import pandas as pd
 from utils.fs import load_csv
 
@@ -20,13 +21,21 @@ args = arg_parser.parse_args()
 def describe(file_path):
     df = load_csv(file_path)
 
+    has_label = args.label_column_name in df
+
+    stats = load_stats(df, args.label_column_name if has_label else None)
+
     pd.set_option('display.max_columns', 500)
     pd.options.display.float_format = '{:.6f}'.format
+
+    print('%d rows, %d columns %s' % (stats.row_count, stats.column_count,
+                                      '(not counting the label column)' if has_label else ''))
+
     if not args.no_contents:
         print(df)
-    print(df.describe(include='all'))
+    print(stats.columns)
 
-    if args.label_column_name in df:
+    if has_label:
         print('\nLabel column: ')
         label_column = df[args.label_column_name]
         print('Value    %')

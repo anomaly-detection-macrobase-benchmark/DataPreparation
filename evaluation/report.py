951d16853bb37126651ece951002adae78f0a3a7
Alex P <alex.pantec@gmail.com>
2019-06-26 11:11:34 +0300

    refactor

--- evaluation/report.py
+++ evaluation/report.py
@@ -8,8 +8,8 @@ from openpyxl import Workbook
 
 from utils.argparse import ArgParser
 from utils.collections import find_first
-from utils.datasets import load_stats
-from utils.fs import load_json, load_csv, list_files
+from utils.datasets import load_dataset
+from utils.fs import load_json, list_files
 import utils.xlsx as xlsx
 from utils.xlsx import autofit, append_blank_rows, pandas_dataframe_to_rows, mark_table, xlref_range_count, xlref, \
     add_image
@@ -30,7 +30,8 @@ results_file_paths = [f for f in list_files(args.result_dir) if f.endswith('.jso
 
 execution_results = [load_json(f) for f in results_file_paths]
 
-data_file_ids = {r['config']['dataset']['id'] for r in execution_results}
+dataset_ids = {r['config']['dataset']['id'] for r in execution_results}
+datasets = {dataset_id: load_dataset(dataset_id, args.data_dir, 'is_anomaly') for dataset_id in dataset_ids}
 
 classifiers = [{
     'classifier': r['config']['classifier']['id'],
@@ -67,27 +68,22 @@ xlsx.add_common_styles(workbook)
 def write_datasets_sheet():
     sheet = workbook.create_sheet('Datasets')
 
-    stats_dict = {}
-    for data_file_id in data_file_ids:
-        dataset = load_csv(os.path.join(args.data_dir, data_file_id))
-        stats = load_stats(dataset, 'is_anomaly')
-        stats_dict[data_file_id] = stats
-
     # main stats table
     header = ['Dataset', 'Samples', 'Dims', 'Anomalies %']
     sheet.append(header)
-    for data_file_id, stats in stats_dict.items():
-        sheet.append([data_file_id, stats.row_count, stats.column_count, stats.anomaly_count / stats.row_count * 100])
-    mark_table(sheet, xlref_range_count(1, 1, len(data_file_ids) + 1, len(header)))
+    for dataset_id, dataset in datasets.items():
+        stats = dataset.stats
+        sheet.append([dataset_id, stats.row_count, stats.column_count, stats.anomaly_count / stats.row_count * 100])
+    mark_table(sheet, xlref_range_count(1, 1, len(dataset_ids) + 1, len(header)))
 
     # column stats for each dataset
-    for data_file_id, stats in stats_dict.items():
+    for dataset_id, dataset in datasets.items():
         append_blank_rows(sheet, 2)
 
-        sheet.append([data_file_id])
+        sheet.append([dataset_id])
         sheet.cell(sheet.max_row, 1).style = 'bold'
         table_start_row = sheet.max_row + 1
-        df_rows = pandas_dataframe_to_rows(stats.columns, top_left_value='Column stats')
+        df_rows = pandas_dataframe_to_rows(dataset.stats.columns, top_left_value='Column stats')
         for r in df_rows:
             sheet.append(r)
         mark_table(sheet, xlref_range_count(table_start_row, 1, len(df_rows), len(df_rows[0])))
--- utils/datasets.py
+++ utils/datasets.py
@@ -1,14 +1,18 @@
+import os
 from collections import namedtuple
 
 import pandas as pd
 
+from utils.fs import load_csv
+
+Dataset = namedtuple('Dataset', ['id', 'stats', 'labels'])
 DatasetStats = namedtuple('DatasetStats', ['row_count', 'column_count', 'anomaly_count', 'columns'])
 
 
-def load_stats(df: pd.DataFrame, label_column_name=None) -> DatasetStats:
+def load_stats(df: pd.DataFrame, label_column_name='is_anomaly') -> DatasetStats:
     col_stats = df.describe(include='all')
     anomaly_count = None
-    if label_column_name:
+    if label_column_name in df:
         label_column = df[label_column_name]
         anomaly_count = label_column.value_counts()[1]
     return DatasetStats(row_count=df.shape[0],
@@ -16,3 +20,15 @@ def load_stats(df: pd.DataFrame, label_column_name=None) -> DatasetStats:
                         columns=col_stats,
                         anomaly_count=anomaly_count
                         )
+
+
+def load_dataset(dataset_id, dir_path='', label_column_name='is_anomaly') -> Dataset:
+    df = load_csv(os.path.join(dir_path, dataset_id))
+
+    labels = None
+    if label_column_name in df:
+        labels = df[label_column_name]
+
+    return Dataset(id=dataset_id,
+                   stats=load_stats(df, label_column_name),
+                   labels=labels)
